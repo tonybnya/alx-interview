2541e9ba565b84440a29f6c96520c229de70dfe3
Tony <nya.tony2010@gmail.com>
2023-10-20 08:33:52 +0100

    Update: '0-stats.py' file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -44,7 +44,6 @@ def log_parsing():
     # sizes is to sum the size of the current batch
     # total_size is to add the sizes of the current batch to all the previous
     codes, counter, sizes, total_size = {}, 0, 0, 0
-    total_size = 0
 
     try:
         for line in sys.stdin:
@@ -60,22 +59,30 @@ def log_parsing():
                 codes[status_code] = codes.get(status_code, 0) + 1
                 sizes += size
 
-            # Check if we reached a batch (10 lines) iteration
-            if counter % 10 == 0:
-                total_size += sizes
-                print(f"File size: {total_size}")
-                for key in sorted(codes):
-                    print(f"{key}: {codes[key]}")
+            # Check if there is data in the current batch
+            if sizes > 0:
+                # Check if we reached a batch (10 lines) iteration
+                if counter % 10 == 0:
+                    total_size += sizes
+                    print(f"File size: {total_size}")
+                    for key in sorted(codes):
+                        print(f"{key}: {codes[key]}")
 
-                # Reset the counter, sizes, and codes for the next batch
-                counter = 0
-                sizes = 0
-                codes = {}
+                    # Reset the counter, sizes, and codes for the next batch
+                    counter = 0
+                    sizes = 0
+                    codes = {}
+            else:
+                # This handles cases of empty file or lines with wrong format
+                total_size = 0
 
     except KeyboardInterrupt:
-        total_size += sizes
+        # Check if there is data in the current batch
+        if sizes > 0:
+            total_size += sizes
         # Print the final statistics if a keyboard interruption occurs
-        print(f"File size: {total_size}")
+        if total_size > 0:
+            print(f"File size: {total_size}")
         for key in sorted(codes):
             print(f"{key}: {codes[key]}")
 
