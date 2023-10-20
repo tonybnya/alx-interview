cd2cdfeb1e60402f937f3d5bf2e83602175b7c1a
Tony <nya.tony2010@gmail.com>
2023-10-20 09:05:35 +0100

    Update: '0-stats.py' file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,19 +14,16 @@ def get_status_code_and_size(line):
     """
     # Define the regex pattern to match the status code
     pattern1 = r'"GET /projects/260 HTTP/1.1" (\d+)'
-
     # Define the regex pattern to match the file size
     pattern2 = r'"GET /projects/260 HTTP/1.1" \d+ (\d+)'
 
     # Use re.search to find the status code of the log line
     match1 = re.search(pattern1, line)
-
     # Use re.search to find the file size of the log line
     match2 = re.search(pattern2, line)
 
     # Get the status code of the log line
     status_code = match1.group(1) if match1 else None
-
     # Get the size of the log line
     size = int(match2.group(1)) if match2 else 0
 
@@ -67,10 +64,10 @@ def log_parsing():
                     for key in sorted(codes):
                         print(f"{key}: {codes[key]}")
 
-                # Reset the counter, sizes, and codes for the next batch
-                counter = 0
+                # Reset the codes, counter, and sizes for the next batch
+                # codes = {}
+                # counter = 0
                 sizes = 0
-                codes = {}
 
         if total_size > 0:
             print(f"File size: {total_size}")
