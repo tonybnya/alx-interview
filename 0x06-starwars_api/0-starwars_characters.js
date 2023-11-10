82431c175bb5d37fdf6489dd818b51a4ce62c9e7
Tony <nya.tony2010@gmail.com>
2023-11-10 15:45:00 +0100

    Update: '0-starwars_characters.js' file

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -24,7 +24,7 @@ const charsURL = `${url}${filmID}`;
 const requester = (url) => {
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
-      if (!error && response.statusCode == 200) {
+      if (!error && response.statusCode === 200) {
         resolve(JSON.parse(body));
       } else {
         reject(error);
