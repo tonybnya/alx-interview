065dc46d75ef91f5d452b7c2c49a916f8e71e510
Tony <nya.tony2010@gmail.com>
2023-11-16 10:00:05 +0100

    Add: '0x07-rotate_2d_matrix' directory

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+
+>>> matrix = [
+...     [1, 2, 3],
+...     [4, 5, 6],
+...     [7, 8, 9]
+... ]
+>>>
+>>> rotate_2d_matrix(matrix)
+>>> print(matrix)
+>>> [
+...     [7, 4, 1],
+...     [8, 5, 2],
+...     [9, 6, 3]
+... ]
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate an n x n 2D Matrix in place.
+
+    :param matrix: n x n 2D matrix
+    :return: nothing
+    """
+    pass
