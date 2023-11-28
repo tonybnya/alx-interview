6d27472d5150aac4ebfe7af940f7a7a14009fd2d
Tony <nya.tony2010@gmail.com>
2023-11-28 16:16:26 +0100

    style: fix some code style with pycodestyle

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,7 +13,8 @@ that returns the perimeter of the island described in `grid`:
     - `grid` is rectangular, with its width and height not exceeding 100
 - The grid is completely surrounded by water
 - There is only one island (or nothing).
-- The island doesn’t have “lakes” (water inside that isn’t connected to the water surrounding the island).
+- The island doesn’t have “lakes”
+(water inside that isn’t connected to the water surrounding the island).
 """
 
 
