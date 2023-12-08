2d1d3b3baa6f278ff4aa27786fd6823845e0bd98
Tony <nya.tony2010@gmail.com>
2023-12-08 08:13:39 +0100

    fix: complete the algorithm file '0-prime_game.py'

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -13,4 +13,29 @@ def isWinner(x, nums):
     :param nums: an array of integers
     :return: the name of the winner or None
     """
-    pass
+    if x < 1 or not nums:
+        return None
+
+    maria, ben = 0, 0
+
+    # generate primes with a limit of the maximum number in nums
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+
+    # filter the number of primes less than n in nums for each round
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        ben += primes_count % 2 == 0
+        maria += primes_count % 2 == 1
+
+    if maria == ben:
+        return None
+
+    return 'Maria' if maria > ben else 'Ben'
