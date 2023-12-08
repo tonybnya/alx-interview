a671d46abbcff743b23d84ae93c6ffa6a828af4a
Tony <nya.tony2010@gmail.com>
2023-12-08 08:10:01 +0100

    feat: add the algorithm file '0-prime_game.py'

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Prime Game
+"""
+
+
+def isWinner(x, nums):
+    """
+    Prime Game
+    Determine who the winner of each game
+
+    :param x: an integer representing the number of rounds
+    :param nums: an array of integers
+    :return: the name of the winner or None
+    """
+    pass
